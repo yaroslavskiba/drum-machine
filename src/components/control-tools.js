8f0ecb379ada64be34074a20442a243e1d3d0012
yaroslav skiba <109871666+yaroslavskiba@users.noreply.github.com>
2023-02-02 22:50:43 +0300

    aghhhhhh

--- src/components/control-tools.js
+++ src/components/control-tools.js
@@ -7,6 +7,10 @@ export default function ControlTools() {
         <input type="checkbox" className='power' id='power' name='active-drum-machine' defaultChecked />
         <label htmlFor='power'>- Power</label>
       </div>
+      <div className='power-tools'>
+        <input type="checkbox" className='power' id='power' name='active-drum-machine' defaultChecked />
+        <label htmlFor='power'>- Bank</label>
+      </div>
     </div>
   );
 }
