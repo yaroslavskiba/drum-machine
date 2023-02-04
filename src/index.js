281bc10d9eda4562bca3a7c09c8a705ff3d23c9e
yaroslav skiba <109871666+yaroslavskiba@users.noreply.github.com>
2023-02-04 20:09:27 +0300

    key and sound

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import store from './store/store';
@@ -8,9 +8,28 @@ import ControlTools from './components/control-tools';
 import { Provider, useDispatch, useSelector } from 'react-redux'
 
 function App() {
-  const soundKey = useSelector(state => state.sound)
-  return (
 
+  useEffect(() => {
+    document.addEventListener("keydown", soundKeyClick);
+    return;
+  })
+
+  function soundMouseClick() {
+    const audio = new Audio()
+    audio.src = 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    audio.play()
+  }
+
+  function soundKeyClick(e) {
+    const key = e.keyCode;
+    if (key === 81) {
+      soundMouseClick()
+    }
+  }
+  return (
+    <>
+      <button onClick={soundMouseClick} onKeyDown={(e) => soundKeyClick(e)}>click</button>
+    </>
   );
 }
 
